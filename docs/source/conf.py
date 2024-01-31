46e93566e6cea62100e102fddd05885a1152fd2f
InSync <122007197+InSyncWithFoo@users.noreply.github.com>
2024-01-31 23:21:02 +0000

    Add default docs/source/conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'template'
+copyright = '2024-now, InSyncWithFoo'
+author = 'InSyncWithFoo'
+version = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+	'sphinx.ext.autodoc',
+	'sphinx.ext.autosectionlabel',
+	'sphinx.ext.doctest',
+	'sphinx.ext.viewcode'
+]
+
+add_module_names = False
+autodoc_member_order = 'bysource'
+autosectionlabel_prefix_document = True
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = []
