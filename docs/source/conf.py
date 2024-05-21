44852deec29d303e23ba4dcdcd2aa3b6df642971
InSync <insyncwithfoo@gmail.com>
2024-05-21 18:33:13 +0700

    Exclude Ruff's `COM812`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# Tutorial: https://www.sphinx-doc.org/en/master/tutorial/index.html
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
